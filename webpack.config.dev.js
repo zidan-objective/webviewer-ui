b346cb6212dec51adbfdec1bf877d8face66f305
Zhijie Zhang <zhijiezhang0124@gmail.com>
2019-08-28 14:09:18 -0700

    Comment out react-hot-loader so that I can test with git-WebViewer

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -25,7 +25,7 @@ module.exports = {
           options: {
             presets: ['@babel/preset-react', '@babel/preset-env'],
             plugins: [
-              'react-hot-loader/babel',
+              // 'react-hot-loader/babel',
               '@babel/plugin-proposal-function-sent',
               '@babel/plugin-proposal-export-namespace-from',
               '@babel/plugin-proposal-numeric-separator',
