153ab6e39b38fb715c4fe81742cf6f672593a123
ZhijieZhang <zhijiezhang0124@gmail.com>
2019-11-01 22:23:46 -0700

    Fix

--- src/event-listeners/onPageNumberUpdated.js
+++ src/event-listeners/onPageNumberUpdated.js
@@ -2,7 +2,5 @@ import actions from 'actions';
 import fireEvent from 'helpers/fireEvent';
 
 export default dispatch => pageNumber => {
-  dispatch(actions.setCurrentPage(pageNumber));
-
   fireEvent('pageChanged', [pageNumber]);
 };
