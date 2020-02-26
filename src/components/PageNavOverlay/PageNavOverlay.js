8c08abc37cbdaf0f96fd9a37ebb9b1f1ff051d24
Liam Ross <liamross.code@gmail.com>
2020-02-25 17:15:55 -0800

    feat(PageNavOverlay): made buttons accessible
    also refactored component to be hooks based in order to avoid race conditions around `docViewer.getPageCount`

--- src/components/PageNavOverlay/PageNavOverlay.js
+++ src/components/PageNavOverlay/PageNavOverlay.js
@@ -1,165 +1,151 @@
-import React from "react";
-import PropTypes from "prop-types";
-import { connect } from "react-redux";
+import Icon from 'components/Icon';
+import core from 'core';
+import { isIOS } from 'helpers/device';
+import getClassName from 'helpers/getClassName';
+import PropTypes from 'prop-types';
+import React, { useEffect, useRef, useState } from 'react';
+import { connect } from 'react-redux';
+import selectors from 'selectors';
+import './PageNavOverlay.scss';
 
-import core from "core";
-import getClassName from "helpers/getClassName";
-import selectors from "selectors";
-import { isIOS } from "helpers/device";
+/* eslint-disable jsx-a11y/click-events-have-key-events */
 
-import Icon from "components/Icon";
+const propTypes = {
+  isLeftPanelDisabled: PropTypes.bool,
+  isLeftPanelOpen: PropTypes.bool,
+  isDisabled: PropTypes.bool,
+  isOpen: PropTypes.bool,
+  currentPage: PropTypes.number,
+  totalPages: PropTypes.number,
+  pageLabels: PropTypes.array.isRequired,
+  allowPageNavigation: PropTypes.bool.isRequired,
+};
 
-import "./PageNavOverlay.scss";
-import goToPrevPage from "../../apis/goToPrevPage";
+function PageNavOverlay(props) {
+  const textInput = useRef(null);
 
-class PageNavOverlay extends React.PureComponent {
-  static propTypes = {
-    isLeftPanelDisabled: PropTypes.bool,
-    isLeftPanelOpen: PropTypes.bool,
-    isDisabled: PropTypes.bool,
-    isOpen: PropTypes.bool,
-    currentPage: PropTypes.number,
-    totalPages: PropTypes.number,
-    pageLabels: PropTypes.array.isRequired,
-    allowPageNavigation: PropTypes.bool.isRequired,
-  };
+  const [isCustomPageLabels, setIsCustomPageLabels] = useState(false);
+  useEffect(() => {
+    const hasCustomLabels = props.pageLabels.some(
+      (label, index) => label !== `${index + 1}`,
+    );
+    setIsCustomPageLabels(hasCustomLabels);
+  }, [props.pageLabels]);
 
-  constructor(props) {
-    super(props);
-    this.textInput = React.createRef();
-    this.state = {
-      input: "",
-      isCustomPageLabels: false
-    };
-  }
-
-  componentDidUpdate(prevProps) {
-    if (prevProps.pageLabels !== this.props.pageLabels) {
-      const isCustomPageLabels = this.props.pageLabels.some(
-        (label, index) => label !== `${index + 1}`
-      );
-      this.setState({ isCustomPageLabels });
+  const [input, setInput] = useState('');
+  useEffect(() => {
+    if (Array.isArray(props.pageLabels) && !props.pageLabels.length) {
+      return;
     }
+    setInput(props.pageLabels[props.currentPage - 1]);
+  }, [props.currentPage, props.pageLabels]);
 
-    if (
-      prevProps.currentPage !== this.props.currentPage ||
-      prevProps.pageLabels !== this.props.pageLabels
-    ) {
-      this.setState({
-        input: this.props.pageLabels[this.props.currentPage - 1]
-      });
-    }
+  const [disabled, setDisabled] = useState({ prev: true, next: false });
+  useEffect(() => {
+    setDisabled(old => {
+      const prev = Number(input) === 1;
+      const next = Number(input) === window.docViewer.getPageCount();
 
-    // if (
-    //   prevProps.totalPages !== this.props.totalPages &&
-    //   !this.props.isDisabled
-    // ) {
-    //   this.setInputWidth();
-    // }
+      if (old.prev === prev && old.next === next) {
+        return old;
+      }
+      return { prev, next };
+    });
+  }, [input]);
 
-    // if (prevProps.isDisabled && !this.props.isDisabled) {
-    //   this.setInputWidth();
-    // }
-  }
-
-  // setInputWidth = () => {
-  //   this.textInput.current.style.width = `${this.props.totalPages.toString()
-  //     .length * 8}px`;
-  // };
-
-  onClick = () => {
+  const onSelectText = () => {
     if (isIOS) {
       setTimeout(() => {
-        this.textInput.current.setSelectionRange(0, 9999);
+        textInput.current.setSelectionRange(0, 9999);
       }, 0);
     } else {
-      this.textInput.current.select();
+      textInput.current.select();
     }
   };
 
-  onChange = e => {
-    if (e.target.value.length > this.props.totalPages.toString().length) {
+  const onChange = e => {
+    if (e.target.value.length > props.totalPages.toString().length) {
       return;
     }
 
-    this.setState({ input: e.target.value });
+    setInput(e.target.value);
   };
 
-  onSubmit = e => {
+  const onSubmit = e => {
     e.preventDefault();
 
-    const { input } = this.state;
-    const isValidInput = input === "" || this.props.pageLabels.includes(input);
+    const isValidInput = input === '' || props.pageLabels.includes(input);
 
     if (isValidInput) {
-      const pageToGo = this.props.pageLabels.indexOf(input) + 1;
+      const pageToGo = props.pageLabels.indexOf(input) + 1;
       core.setCurrentPage(pageToGo);
-    } else {
-      this.textInput.current.blur();
-    }
-  };
-
-  onBlur = () => {
-    const { currentPage, pageLabels } = this.props;
-
-    this.setState({ input: pageLabels[currentPage - 1] });
-  };
-
-  render() {
-    const { isDisabled, isLeftPanelOpen, isLeftPanelDisabled, currentPage, totalPages, allowPageNavigation } = this.props;
-    if (isDisabled) {
-      return null;
     }
 
-    const className = getClassName(`Overlay PageNavOverlay`, this.props);
+    onSelectText();
+  };
 
-    const inputWidth = (this.state.input.length) * 8;
+  const onBlur = () => {
+    setInput(props.pageLabels[props.currentPage - 1]);
+  };
 
-    return (
-      <div className={className} data-element="pageNavOverlay">
-        <div
-          className="down-arrow-container"
-          onClick={() =>
-            window.docViewer.setCurrentPage(
-              Math.max(window.docViewer.getCurrentPage() - 1, 1),
-            )
-          }
-        >
-          <Icon className="down-arrow" glyph="icon-chevron-left" />
-        </div>
-        <div className="formContainer" onClick={this.onClick}>
-          <form onSubmit={this.onSubmit} onBlur={this.onBlur}>
-            <input
-              ref={this.textInput}
-              type="text"
-              value={this.state.input}
-              onChange={this.onChange}
-              tabIndex={-1}
-              disabled={!allowPageNavigation}
-              style={{ width: inputWidth }}
-            />
-            {this.state.isCustomPageLabels
-              ? ` (${currentPage}/${totalPages})`
-              : ` / ${totalPages}`}
-          </form>
-        </div>
-        <div
-          className="down-arrow-container"
-          onClick={() =>
-            window.docViewer.setCurrentPage(
-              Math.min(
-                window.docViewer.getCurrentPage() + 1,
-                window.docViewer.getPageCount(),
-              ),
-            )
-          }
-        >
-          <Icon className="down-arrow" glyph="icon-chevron-right" />
-        </div>
+  const inputWidth = input.length * 8;
+
+  const className = getClassName(`Overlay PageNavOverlay`, props);
+
+  return (
+    <div className={className} data-element="pageNavOverlay">
+      <button
+        type="button"
+        className="down-arrow-container down-arrow-container--left"
+        disabled={disabled.prev}
+        onClick={() =>
+          window.docViewer.setCurrentPage(
+            Math.max(window.docViewer.getCurrentPage() - 1, 1),
+          )
+        }
+      >
+        <Icon className="down-arrow" glyph="icon-chevron-left" />
+      </button>
+      <div
+        className="formContainer"
+        onClick={onSelectText}
+        role="button"
+        tabIndex={-1}
+      >
+        <form onSubmit={onSubmit} onBlur={onBlur}>
+          <input
+            ref={textInput}
+            type="number"
+            value={input}
+            onChange={onChange}
+            disabled={!props.allowPageNavigation}
+            style={{ width: inputWidth }}
+          />
+          {isCustomPageLabels
+            ? ` (${props.currentPage}/${props.totalPages})`
+            : ` / ${props.totalPages}`}
+        </form>
       </div>
-    );
-  }
+      <button
+        type="button"
+        className="down-arrow-container down-arrow-container--right"
+        disabled={disabled.next}
+        onClick={() =>
+          window.docViewer.setCurrentPage(
+            Math.min(
+              window.docViewer.getCurrentPage() + 1,
+              window.docViewer.getPageCount(),
+            ),
+          )
+        }
+      >
+        <Icon className="down-arrow" glyph="icon-chevron-right" />
+      </button>
+    </div>
+  );
 }
+
+PageNavOverlay.propTypes = propTypes;
 
 const mapStateToProps = state => ({
   isDisabled: selectors.isElementDisabled(state, 'pageNavOverlay'),
